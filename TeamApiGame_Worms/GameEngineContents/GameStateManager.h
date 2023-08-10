e2356fc82c4cd061da92015fd72793a681b94fc3
DahyunKim <risehyun01@gmail.com>
2023-08-10 21:45:52 +0900

    김다현 : Pull 하기 위한 업데이트 (GameStateManager 초안 작업 일부 포함)

--- TeamApiGame_Worms/GameEngineContents/GameStateManager.h
+++ TeamApiGame_Worms/GameEngineContents/GameStateManager.h
@@ -20,5 +20,61 @@ private:
 
 	static GameStateManager GameState;
 
+	// Getter
+
+	float GetRoundTime() const
+	{
+		return m_fRoundTime;
+	}
+
+	float GetTurnTime() const
+	{
+		return m_fTurnTime;
+	}
+
+	int GetWinConditions() const
+	{
+		return m_iWinConditions;
+	}
+
+	int GetWormMaxHp() const
+	{
+		return m_iWormMaxHp;
+	}
+
+	int GetWormSelectMode() const
+	{
+		return m_iWormSelectMode;
+	}
+
+	bool GetUseTeleport() const
+	{
+		return m_bUseTeleport;
+	}
+
+		
+
+	// Setter
+	// 매개변수로 enum을 받는게 더 나을 수도
+	void SetRoundTime()
+	{
+	
+	}
+
+
+
+
+
+
+	//class GameSettingData
+	//{
+	float	m_fRoundTime = 0.0f;
+	float	m_fTurnTime = 0.0f;
+	int		m_iWinConditions = 0;
+	int		m_iWormMaxHp = 0;
+	int		m_iWormSelectMode = 0;	// enum 타입으로 선언하는 거 고려해보기
+	bool	m_bUseTeleport = false;
+	//};
+
 };
 
---
+++ TeamApiGame_Worms/GameEngineContents/UI_Inventory.cpp
@@ -0,0 +1,20 @@
+#include "UI_Inventory.h"
+
+UI_Inventory::UI_Inventory()
+{
+}
+
+UI_Inventory::~UI_Inventory()
+{
+}
+
+void UI_Inventory::Start()
+{
+
+
+
+}
+
+void UI_Inventory::Update(float _Delta)
+{
+}
