357229d6573176ef721ae77d4875d406cffc908f
aruel7714 <harunia7714@daum.net>
2023-08-17 16:36:17 +0900

    김성원 : Diving 상태 추가 (조건 아직 추가 안함)

--- TeamApiGame_Worms/GameEngineContents/Player_State.cpp
+++ TeamApiGame_Worms/GameEngineContents/Player_State.cpp
@@ -218,6 +218,12 @@ void Player::FallingUpdate(float _Delta)
 		ChangeState(PlayerState::Idle);
 		return;
 	}
+
+	// 물에 닿으면 Diving 상태
+	if (false)
+	{
+		ChangeState(PlayerState::Diving);
+	}
 }
 
 void Player::DamagingStart()
@@ -1660,4 +1666,23 @@ void Player::WinStart()
 void Player::WinUpdate(float _Delta)
 {
 
+}
+
+void Player::DivingStart()
+{
+	ChangeAnimationState("Diving1");
+}
+void Player::DivingUpdate(float _Delta)
+{
+	if (MainRenderer->IsAnimationEnd())
+	{
+		if (MainRenderer->IsAnimation("Left_Diving1") || MainRenderer->IsAnimation("Right_Diving1"))
+		{
+			ChangeAnimationState("Diving2");
+		}
+		else if(MainRenderer->IsAnimation("Left_Diving2") || MainRenderer->IsAnimation("Right_Diving2"))
+		{
+			ChangeAnimationState("Diving1");
+		}
+	}
 }
