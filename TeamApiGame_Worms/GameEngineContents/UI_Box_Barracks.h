4bc4515d2d425b80bf8dd12b9af17f743be11c01
DahyunKim <risehyun01@gmail.com>
2023-08-22 16:25:35 +0900

    김다현 : 로비 UI 2차 수정

--- TeamApiGame_Worms/GameEngineContents/UI_Box_Barracks.h
+++ TeamApiGame_Worms/GameEngineContents/UI_Box_Barracks.h
@@ -2,8 +2,11 @@
 #include "UI_Button.h"
 #include <vector>
 
+class UI_Box_Teams;
 class UI_Box_Barracks : public UI_Button
 {
+	friend UI_Box_Teams;
+
 public:
 	UI_Box_Barracks();
 	~UI_Box_Barracks();
@@ -22,6 +25,11 @@ public:
 
 	void ChangeState(BUTTON_STATE _ButtonState);
 
+	UI_Box_Barracks* GetBoxBarracks() const
+	{
+		return BoxBarracksUI;
+	}
+
 	int m_SelectIndex = 0;
 
 protected:
@@ -29,6 +37,9 @@ protected:
 	void Update(float _Delta) override;
 
 private:
+
+	static UI_Box_Barracks* BoxBarracksUI;
+
 	std::vector<GameEngineRenderer*> AllTeamRenderers;
 	std::vector<float4> AllTeamRenderPos;
 
